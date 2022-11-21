aabf2470d5187ae1ae77c323a45efe4d95331e20
YasinCap <yasin.gumus@capgemini.com>
2022-11-21 10:39:04 +0100

    logo

--- Navbar.js
+++ Navbar.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Navbar() {
     return(
         <nav className='flex space-x-7 py-4'>
-            <img src="../components/images" />
+            <img src="https://i.imgur.com/6r1X7bF.png" />
              <p className='text-2xl font-bold text-grey-800'>Gemeente Rotterbosch</p>
              <Link href='/blogs/blog35'><p className='text-2xl font-bold text-grey-800'>Recente Blog</p>
              </Link>
