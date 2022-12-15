7f1937ec9ab37ece4afa8e62404df82c7e0c355e
YasinCap <yasin.gumus@capgemini.com>
2022-12-15 16:46:44 +0100

    Navbar aangepast

--- Navbar.js
+++ Navbar.js
@@ -5,13 +5,15 @@ export default function Navbar() {
   return (
     <nav className="flex space-x-7 py-4">
       <img className="w-20 h-12" src="https://i.imgur.com/6r1X7bF.png" />
-      <p className="text-2xl font-bold text-grey-800 hover:text-[#081453]">
-        Gemeente Rotterbosch
-      </p>
+
       <Link
         className="hover:text-[#081453] rounded transition duration-300 ease-in-out"
-        href="/blogs/blog35"
-      ></Link>
+        href="/"
+      >
+        <p className="text-2xl font-bold text-grey-800 hover:text-[#081453]">
+          Gemeente Rotterbosch
+        </p>
+      </Link>
     </nav>
   );
 }
