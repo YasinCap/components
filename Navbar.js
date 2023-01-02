aa6ba1d9a50b1e58c37ee02f7ad7cbdba449eacb
YasinCap <yasin.gumus@capgemini.com>
2023-01-02 17:05:36 +0100

    Navbar aangepast

--- Navbar.js
+++ Navbar.js
@@ -2,6 +2,9 @@ import React from "react";
 import Link from "next/link";
 
 export default function Navbar() {
+  function add(a, b) {
+    return a - b;
+  }
   return (
     <nav className="flex space-x-7 py-4">
       <img className="w-20 h-12" src="https://i.imgur.com/6r1X7bF.png" />
@@ -10,7 +13,11 @@ export default function Navbar() {
         className="hover:text-[#081453] rounded transition duration-300 ease-in-out"
         href="/"
       >
-        <p className="text-2xl font-bold text-grey-800 hover:text-[#081453]">
+        <p className="test">hooooi{add(2, 3)}</p>
+        <p
+          data-testid="navbarTitle"
+          className="text-2xl font-bold text-grey-800 hover:text-[#081453]"
+        >
           Gemeente Rotterbosch
         </p>
       </Link>
